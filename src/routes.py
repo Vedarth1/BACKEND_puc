5e3efbdcd4061d725057752d6931eab418687547
AnamikaKumbhare <kumbhare.anamika@gmail.com>
2024-08-23 20:01:48 +0530

    create new gas_detection controller

--- src/routes.py
+++ src/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from src.controllers.auth_controller import users
 from src.controllers.puc_validation_controller import puc_validation
 from src.controllers.image_processing import image_processing
-
+from src.controllers.gas_detection import gas_detection
 # main blueprint to be registered with application
 api = Blueprint('api', __name__)
 
@@ -10,3 +10,4 @@ api = Blueprint('api', __name__)
 api.register_blueprint(users, url_prefix="/users")
 api.register_blueprint(puc_validation,url_prefix="/puc")
 api.register_blueprint(image_processing,url_prefix="/puc")
+api.register_blueprint(gas_detection,url_prefix="/puc")
